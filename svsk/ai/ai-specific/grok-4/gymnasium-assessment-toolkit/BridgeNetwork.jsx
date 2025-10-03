1793277b80935096753069b3131bf270d5c2dbd4
MarkusIsaksson1982 <mjisaksson@gmail.com>
2025-10-03 16:32:59 +0200

    Delete svsk directory

--- svsk/ai/ai-specific/grok-4/gymnasium-assessment-toolkit/BridgeNetwork.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ForceGraph2D from 'react-force-graph-2d';
-
-const BridgeNetwork = ({ bridges }) => {
-  const nodes = [{ id: 'Descriptor' }, ...Object.keys(bridges).map(id => ({ id }))];
-  const links = Object.entries(bridges).map(([subj, {raw}]) => ({ source: 'Descriptor', target: subj, value: raw }));
-  return <ForceGraph2D graphData={{ nodes, links }} linkWidth={l => l.value * 5} />;
-};
